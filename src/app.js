1618b4cc69f036415cb552315a4ed338d8cbd916
fernandosg <fsg_tmp@hotmail.com>
2017-02-03 23:49:49 -0600

    Se añade inicioCalibracion para el proceso de calibracion de JSArtoolkit.

--- src/app.js
+++ src/app.js
@@ -155,6 +155,27 @@ Memorama.prototype.fnAfter=function(puntero){
   }
 }
 
+Memorama.prototype.inicioCalibarcion=function(){
+  this.objetos=[];
+  this.colores=["rgb(34, 208, 6)","rgb(25, 11, 228)","rgb(244, 6, 6)","rgb(244, 232, 6)"];
+
+  //CREACION DE OBJETOS A SELECCIONAR PARA CALIBRAR
+  limite_renglon=Math.floor(this.cantidad_cartas/2)+1;
+  tamano_elemento=80;
+  margenes_espacio=(this.WIDTH_CANVAS-(tamano_elemento*limite_renglon))/limite_renglon;
+   for(var x=1,cont_fila=1,pos_y=-100,fila_pos=x,pos_x=-200;x<=this.cantidad_cartas;x++,pos_y=((fila_pos>=limite_renglon-1) ? pos_y+120+50 : pos_y) ,fila_pos=((fila_pos>=limite_renglon-1) ? 1 : fila_pos+1),pos_x=(fila_pos==1 ? -200 : (pos_x+margenes_espacio+tamano_elemento))){
+      var elemento=new parent.Elemento(tamano_elemento,tamano_elemento,new THREE.PlaneGeometry(tamano_elemento,tamano_elemento));
+      elemento.init();
+      elemento.etiqueta(this.colores[x-1]);
+      elemento.position({x:pos_x,y:pos_y,z:-600});
+      elemento.calculoOrigen();
+      this.objetos.push(elemento);
+      elemento.definirBackground(this.colores[x-1]);
+      this.observador.suscribir("colision",this.objetos[this.objetos.length-1]);
+      this.anadir(elemento.get());
+  }
+}
+
 Memorama.prototype.calibracion=function(){
     if(calibrar){
       threshold_total=0;

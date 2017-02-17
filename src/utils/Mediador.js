32bbda196c400ea875f245b0a22c63eef92f7b05
fernandosg <fsg_tmp@hotmail.com>
2017-02-16 21:33:17 -0600

    Agregados comentarios al documento Mediador.

--- src/utils/Mediador.js
+++ src/utils/Mediador.js
@@ -1,15 +1,41 @@
+/**
+ * @file Mediador
+ * @author Fernando Segura Gómez, Twitter: @fsgdev
+ * @version 0.1
+ */
+
+ /**
+  * Clase Mediador
+  * @class
+  * @constructor
+ */
 function Mediador(){
 	this.lista_eventos={};
 };
 
+
+/**
+ * @function suscribir
+ * @summary Permite suscribir un evento a la escucha que el Mediador ocupara para comunicar a ciertos objetos que esten escuchando a dicho evento
+ * @param {String} evento - El evento que el Mediador ocupara para comunicarse con el objeto añadido.
+ * @param {Elemento} objeto - El objeto el cual puede tener comunicación con el Mediador con un evento especifico.
+*/
 Mediador.prototype.suscribir=function(evento,objeto){
 	if(!this.lista_eventos[evento]) this.lista_eventos[evento]=[];
 	if(this.lista_eventos[evento].indexOf(objeto)==-1){
 		this.lista_eventos[evento].push(objeto);
 	}
-	console.log("Suscribiendo");
 }
 
+
+/**
+ * @function comunicar
+ * @summary Evento comunicar
+ * @param {String} evento
+ * @param {Elemento} objeto
+ * @param {Function} callback
+ * @param {extras} Object
+*/
 Mediador.prototype.comunicar=function(evento,objeto,callback,extras){
 	if(!this.lista_eventos[evento]) return;
 	for(var i=0;i<this.lista_eventos[evento].length;i++){
@@ -18,6 +44,15 @@ Mediador.prototype.comunicar=function(evento,objeto,callback,extras){
 	}
 }
 
+
+/**
+ * @function comunicarParticular
+ * @summary Evento comunicar
+ * @param {String} evento
+ * @param {Elemento} objeto
+ * @param {Function} callback
+ * @param {extras} Object
+*/
 Mediador.prototype.comunicarParticular=function(evento,objeto,compara,callback){
 	if(!this.lista_eventos[evento]) return;
 	var pos=this.lista_eventos[evento].indexOf(objeto);
@@ -27,6 +62,13 @@ Mediador.prototype.comunicarParticular=function(evento,objeto,compara,callback){
 	callback(this.lista_eventos[evento][pos].dispatch(compara),extras);
 }
 
+
+/**
+ * @function baja
+ * @summary Evento comunicar
+ * @param {String} evento
+ * @param {Elemento} objeto 
+*/
 Mediador.prototype.baja=function(evento,objeto){
 	if(this.lista_eventos[evento].indexOf(objeto)==-1) return;
 	this.lista_eventos[evento].splice(this.lista_eventos[evento].indexOf(objeto),1);
